096632ecdaac66e6899863d99f11285a2b6d13a7
sawgit <scottalanw@gmail.com>
2020-12-18 13:14:01 -0500

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,114 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo[300],
+        appBar: AppBar(
+          toolbarHeight: 55.0,
+          title: Center(
+            child: Text(
+              'Scotts Flutter Card',
+              style: TextStyle(
+                fontFamily: 'Rajdhani',
+                fontSize: 34.0,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.indigo[400],
+        ),
+        body: SafeArea(
+          child: ScottsCard(),
+        ),
+      ),
+    );
+  }
+}
+
+class ScottsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/meSquare.jpg'),
+            radius: 89.0,
+          ),
+          Text(
+            'Scott Alan Williams',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 34.0,
+              fontFamily: 'Merienda',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'Front-End / Flutter Developer',
+            style: TextStyle(
+              color: Colors.grey[300],
+              fontSize: 21.0,
+              fontFamily: 'Rajdhani',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+              width: 155.0,
+              height: 55.0,
+              child: Divider(
+                color: Colors.indigo[200],
+              )),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 21.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
+                size: 34.0,
+                color: Colors.indigo[300],
+              ),
+              title: Text(
+                '321-210-9243',
+                style: TextStyle(
+                  color: Colors.indigo[300],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21.0,
+                  fontFamily: 'Rajdhani',
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 21.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                size: 34.0,
+                color: Colors.indigo[300],
+              ),
+              title: Text(
+                'scottalanw@gmail.com',
+                style: TextStyle(
+                  color: Colors.indigo[300],
+                  fontWeight: FontWeight.bold,
+                  fontSize: 21.0,
+                  fontFamily: 'Rajdhani',
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
